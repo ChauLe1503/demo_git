249c517feb69551b865d4abddf37c442f657de9c
Dang2306 <ltchau15031996@gmail.com>
2024-08-07 15:51:02 +0700

    commit demo

--- src/java/TestGit.java
+++
@@ -1,9 +0,0 @@
-package java;
-
-public class TestGit {
-  public static void main(String[] args) {
-    System.out.println("Hello");
-    System.out.println("Hello");
-    System.out.println("Hello");
-  }
-}
