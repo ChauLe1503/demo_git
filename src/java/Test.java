6aaf4b4056ac1d70511e78567f3377c4b75108e9
Dang2306 <ltchau15031996@gmail.com>
2024-08-07 15:32:18 +0700

    name_96 commit

--- src/java/Test.java
+++ src/java/Test.java
@@ -2,6 +2,15 @@ package java;
 
 public class Test {
   public static void main(String[] args) {
-
+    System.out.println("abcbccbc");
+    System.out.println("abcbccbc");
+    System.out.println("abcbccbc");
+    System.out.println("abcbccbc");
+    System.out.println("abcbccbc");
+    System.out.println("abcbccbc");
+    System.out.println("abcbccbc");
+    System.out.println("abcbccbc");
+    System.out.println("abcbccbc");
+    System.out.println("abcbccbc");
   }
 }
